6961a678b18b6b221db0a620da3af2dc277e9e97
Kevyn SOLA <kevyn.sola@gmail.com>
2022-12-14 11:02:41 +0100

    add search page

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    #[Route('/search', name: 'app_search')]
+    public function index(): Response
+    {
+        return $this->render('search/index.html.twig');
+    }
+}
