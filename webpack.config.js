3ba77ed9e5da13d7d44b01ea309e21ad3c9b19c0
Kevyn SOLA <kevyn.sola@gmail.com>
2022-12-16 10:15:22 +0100

    replace scss to css entry encore

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('showVideo', './assets/styles/showVideo.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
