cca6daa4fd8a434becacef2fd33eadca51b09fec
Kevyn SOLA <kevyn.sola@gmail.com>
2022-12-21 17:04:26 +0100

    add entity and fixtures

---
+++ src/Entity/Advertisement.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdvertisementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdvertisementRepository::class)]
+class Advertisement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $poster = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $linkTo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getLinkTo(): ?string
+    {
+        return $this->linkTo;
+    }
+
+    public function setLinkTo(string $linkTo): self
+    {
+        $this->linkTo = $linkTo;
+
+        return $this;
+    }
+}
---
+++ src/Repository/AdvertisementRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Advertisement;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Advertisement>
+ *
+ * @method Advertisement|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Advertisement|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Advertisement[]    findAll()
+ * @method Advertisement[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AdvertisementRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Advertisement::class);
+    }
+
+    public function save(Advertisement $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Advertisement $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Advertisement[] Returns an array of Advertisement objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Advertisement
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
