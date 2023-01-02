cd435a0985c26ef10863efd70f61a6746dec43b5
lucianpurcaru <[lucian.purcaru@gmail.com](mailto:lucian.purcaru@gmail.com)>
2023-01-02 22:21:06 +0100

    update

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,7 +36,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-            
+
             $this->addFlash(
                 'succes',
                 'Bienvenue!'
