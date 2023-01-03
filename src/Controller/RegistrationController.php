3291bd980c38a90b03d780875e79bb0e6efacf50
lucianpurcaru <[lucian.purcaru@gmail.com](mailto:lucian.purcaru@gmail.com)>
2023-01-03 12:24:04 +0100

    update changes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -14,7 +14,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/enregistrement', name: 'app_register')]
+    #[Route('/inscription', name: 'app_register')]
     public function register(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
@@ -38,10 +38,10 @@ class RegistrationController extends AbstractController
             // do anything else you need here, like send an email
 
             $this->addFlash(
-                'succes',
-                'Bienvenue!'
+                'success',
+                'Le compte a été crée! Veuillez vous connecter ci-dessous!'
             );
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
