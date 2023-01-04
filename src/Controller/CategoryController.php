c6cb7216aff698a9072a2354296380dbbf66f2f0
Severine Helene Vilaine <severine.vilaine@gmail.com>
2023-01-04 15:01:17 +0100

    OK

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
