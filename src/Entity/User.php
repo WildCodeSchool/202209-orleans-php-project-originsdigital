122eccf1236c42ea066af0a27fecc47cbdcf0497
Lucian Purcaru <97845722+lucianpurcaru@users.noreply.github.com>
2023-01-05 10:34:59 +0100

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,7 +16,6 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[Vich\Uploadable]
-
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
