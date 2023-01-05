da302f718a8c73da6800bc84e913428b948a64ab
lucianpurcaru <[lucian.purcaru@gmail.com](mailto:lucian.purcaru@gmail.com)>
2023-01-05 21:08:53 +0100

    vue done

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -16,9 +16,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProfileController extends AbstractController
 {
     #[Route('/profil', name: 'app_profile')]
-    public function index(): Response
+    public function index(
+    ): Response
     {
+        $user = $this->getUser();
+
         return $this->render('profile/index.html.twig', [
+            'user'=> $user
         ]);
     }
 
