e10d6a912f9493695eea2bdba40fcdc474fb0f82
Kevyn SOLA <kevyn.sola@gmail.com>
2023-01-09 17:01:29 +0100

    clean progress bar and make responsive one by one carrousel

--- assets/carrousel.js
+++ assets/carrousel.js
@@ -10,102 +10,38 @@ document.addEventListener("click", (e) => {
     }
 });
 
-const throttleProgressBar = throttle(() => {
-    document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
-}, 250);
-window.addEventListener("resize", throttleProgressBar);
+let screenWidth = window.innerWidth
+window.addEventListener("resize", (event) => {
+    screenWidth = window.innerWidth
+});
 
-document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
-
-function calculateProgressBar(progressBar) {
-    progressBar.innerHTML = "";
-    const slider = progressBar
-        .closest(".row-carrousel")
-        .querySelector(".slider");
-    const itemCount = slider.children.length;
-    const itemsPerScreen = parseInt(
-        getComputedStyle(slider).getPropertyValue("--items-per-screen")
-    );
+function onHandleClick(handle) {
+    const slider = handle.closest(".bloc-carrousel").querySelector(".slider");
     let sliderIndex = parseInt(
         getComputedStyle(slider).getPropertyValue("--slider-index")
     );
-    const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen);
+    const sliderItems = handle.parentElement.childNodes.length
+    let sliderBuffer = 0
 
-    if (sliderIndex >= progressBarItemCount) {
-        slider.style.setProperty("--slider-index", progressBarItemCount - 1);
-        sliderIndex = progressBarItemCount - 1;
+    if (screenWidth > 1000) {
+        sliderBuffer = 0;
+    } else if (screenWidth > 768) {
+        sliderBuffer = 1;
+    } else if (screenWidth > 574) {
+        sliderBuffer = 2;
+    } else {
+        sliderBuffer = 3
     }
 
-    for (let i = 0; i < progressBarItemCount; i++) {
-        const barItem = document.createElement("div");
-        barItem.classList.add("progress-item");
-        if (i === sliderIndex) {
-            barItem.classList.add("active");
-        }
-        progressBar.append(barItem);
-    }
-}
-
-function onHandleClick(handle) {
-    const progressBar = handle
-        .closest(".row-carrousel")
-        .querySelector(".progress-bar");
-    const slider = handle.closest(".bloc-carrousel").querySelector(".slider");
-    const sliderIndex = parseInt(
-        getComputedStyle(slider).getPropertyValue("--slider-index")
-    );
-    const progressBarItemCount = progressBar.children.length;
     if (handle.classList.contains("left-handle")) {
-        if (sliderIndex - 1 < 0) {
-            slider.style.setProperty(
-                "--slider-index",
-                progressBarItemCount - 1
-            );
-            progressBar.children[sliderIndex].classList.remove("active");
-            progressBar.children[progressBarItemCount - 1].classList.add(
-                "active"
-            );
-        } else {
+        if (sliderIndex > 0) {
             slider.style.setProperty("--slider-index", sliderIndex - 1);
-            progressBar.children[sliderIndex].classList.remove("active");
-            progressBar.children[sliderIndex - 1].classList.add("active");
         }
     }
 
     if (handle.classList.contains("right-handle")) {
-        if (sliderIndex + 1 >= progressBarItemCount) {
-            slider.style.setProperty("--slider-index", 0);
-            progressBar.children[sliderIndex].classList.remove("active");
-            progressBar.children[0].classList.add("active");
-        } else {
+        if (sliderIndex < sliderItems + sliderBuffer - 1) {
             slider.style.setProperty("--slider-index", sliderIndex + 1);
-            progressBar.children[sliderIndex].classList.remove("active");
-            progressBar.children[sliderIndex + 1].classList.add("active");
         }
     }
-}
-
-function throttle(cb, delay = 1000) {
-    let shouldWait = false;
-    let waitingArgs;
-    const timeoutFunc = () => {
-        if (waitingArgs == null) {
-            shouldWait = false;
-        } else {
-            cb(...waitingArgs);
-            waitingArgs = null;
-            setTimeout(timeoutFunc, delay);
-        }
-    };
-
-    return (...args) => {
-        if (shouldWait) {
-            waitingArgs = args;
-            return;
-        }
-
-        cb(...args);
-        shouldWait = true;
-        setTimeout(timeoutFunc, delay);
-    };
 }
