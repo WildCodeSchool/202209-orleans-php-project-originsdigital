21327a3dfd04130f5ff265a99f73d2c5c5a5f094
Severine Helene Vilaine <severine.vilaine@gmail.com>
2023-01-16 23:22:43 +0100

    Video format works fine on Chromium, not FF

--- src/DataFixtures/VideoFixtures.php
+++ src/DataFixtures/VideoFixtures.php
@@ -9,6 +9,7 @@ use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 class VideoFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -20,7 +21,11 @@ class VideoFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
+        $files = glob('/videos/*');
 
+        foreach ($files as $file) {
+            copy($file, __DIR__ . '/../../public/uploads/videos/');
+        }
         foreach (CategoryFixtures::CATEGORIES as $key => $categoryName) {
             for ($j = 1; $j <= self::NBR_VIDEOS; $j++) {
                 $video = new Video();
@@ -29,7 +34,7 @@ class VideoFixtures extends Fixture implements DependentFixtureInterface
                 $video->setCategory($this->getReference('category_' . $key));
                 $video->setView($faker->numberBetween(1, 200));
                 $video->setDuration($faker->numberBetween(2, 5));
-                $video->setVideo($this->$video(rand(1, 3)));
+                $video->setVideo('video' . (rand(1, 3) . '.mp4'));
                 $manager->persist($video);
             }
         }
