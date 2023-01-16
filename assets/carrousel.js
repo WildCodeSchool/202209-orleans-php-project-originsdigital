9b559062f3849debacb7a240e85836212049d9df
Kevyn SOLA <kevyn.sola@gmail.com>
2023-01-16 09:49:17 +0100

    refacto js and scss

--- assets/carrousel.js
+++ assets/carrousel.js
@@ -1,47 +1,22 @@
-document.addEventListener("click", (e) => {
-    let handle;
-    if (e.target.matches(".handle")) {
-        handle = e.target;
-    } else {
-        handle = e.target.closest(".handle");
-    }
-    if (handle != null) {
-        onHandleClick(handle);
-    }
-});
+const handles = document.querySelectorAll('.handle');
+for (let handle of handles) {
+    handle.addEventListener("click", (e) => {
+        const slider = handle.parentElement.querySelector('.slider');
+        let sliderIndex = parseInt(
+            getComputedStyle(slider).getPropertyValue("--slider-index")
+        );
 
-let screenWidth = window.innerWidth
-window.addEventListener("resize", (event) => {
-    screenWidth = window.innerWidth
-});
+        const sliderItems = slider.querySelectorAll('a').length;
+        let itemsPerScreen = parseInt(
+            getComputedStyle(slider).getPropertyValue("--items-per-screen")
+        );
 
-function onHandleClick(handle) {
-    const slider = handle.closest(".bloc-carrousel").querySelector(".slider");
-    let sliderIndex = parseInt(
-        getComputedStyle(slider).getPropertyValue("--slider-index")
-    );
-    const sliderItems = handle.parentElement.childNodes.length
-    let sliderBuffer = 0
-
-    if (screenWidth > 1000) {
-        sliderBuffer = 0;
-    } else if (screenWidth > 768) {
-        sliderBuffer = 1;
-    } else if (screenWidth > 574) {
-        sliderBuffer = 2;
-    } else {
-        sliderBuffer = 3
-    }
-
-    if (handle.classList.contains("left-handle")) {
-        if (sliderIndex > 0) {
+        if (handle.classList.contains("left-handle") && sliderIndex > 0) {
             slider.style.setProperty("--slider-index", sliderIndex - 1);
         }
-    }
 
-    if (handle.classList.contains("right-handle")) {
-        if (sliderIndex < sliderItems + sliderBuffer - 1) {
+        if (handle.classList.contains("right-handle") && sliderIndex < sliderItems - itemsPerScreen) {
             slider.style.setProperty("--slider-index", sliderIndex + 1);
         }
-    }
+    });
 }
