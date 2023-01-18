062d7c3423319e139a4336d4c8d18c7fdeda1847
Kevyn SOLA <kevyn.sola@gmail.com>
2023-01-18 17:25:47 +0100

    trad and route

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/ad')]
+#[Route('/admin/ad')]
 class AdController extends AbstractController
 {
     #[Route('/', name: 'app_ad_index', methods: ['GET'])]
