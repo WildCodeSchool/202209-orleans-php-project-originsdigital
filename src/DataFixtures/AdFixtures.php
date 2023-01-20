d1ce39ded3ffac2be9eb7702c4888f4dbee63bdf
Kevyn SOLA <kevyn.sola@gmail.com>
2023-01-20 11:06:31 +0100

    fix ads resize

--- src/DataFixtures/AdFixtures.php
+++ src/DataFixtures/AdFixtures.php
@@ -9,6 +9,13 @@ use Doctrine\Persistence\ObjectManager;
 class AdFixtures extends Fixture
 {
     public const ADS_IMG = 'https://jeuxvideo.rds.ca/wp-content/uploads/sites/2/2020/06/LOL_PROMOART_4.jpg';
+    /* public const ADS_IMG = [
+        'Among-us' => '',
+        'Horizon-zero-dawn' => '',
+        'Rocket-league' => '',
+        'Ps5' => '',
+        'Warcraft' => ''
+    ]; */
 
     public function load(ObjectManager $manager): void
     {
