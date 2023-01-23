d48f8c453d4052daee81f6fedf2385b3959a222d
lucianpurcaru <[lucian.purcaru@gmail.com](mailto:lucian.purcaru@gmail.com)>
2023-01-23 12:05:17 +0100

    favorite  added

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Video;
-use App\Repository\AdvertRepository;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Repository\VideoRepository;
+use App\Repository\AdvertRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -30,5 +32,23 @@ class VideoController extends AbstractController
             'video' => $video,
             'ads' => $randAds,
         ]);
+    }
+
+    #[Route('/{id}/favoris', methods: ['GET','POST'], name: 'app_video_favoris')]
+    public function addToFavorite(Video $video, UserRepository $userRepository): Response
+    {
+
+        /** @var \App\Entity\User */
+        $user = $this->getUser();
+
+        if ($user->isInFavorite($video)) {
+            $user->removeFavorite($video);
+        } else {
+            $user->addFavorite($video);
+        }
+
+        $userRepository->save($user, true);
+
+        return $this->redirectToRoute('app_video_show', ['video' => $video->getId()], Response::HTTP_SEE_OTHER);
     }
 }
