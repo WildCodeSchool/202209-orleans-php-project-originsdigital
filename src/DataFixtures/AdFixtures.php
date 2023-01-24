ae8d3882c9ce6973e1d6ec05523f986492a07ab7
Kevyn SOLA <kevyn.sola@gmail.com>
2023-01-24 12:07:35 +0100

    add ads and resize

--- src/DataFixtures/AdFixtures.php
+++ src/DataFixtures/AdFixtures.php
@@ -8,21 +8,20 @@ use Doctrine\Persistence\ObjectManager;
 
 class AdFixtures extends Fixture
 {
-    public const ADS_IMG = 'https://jeuxvideo.rds.ca/wp-content/uploads/sites/2/2020/06/LOL_PROMOART_4.jpg';
-    /* public const ADS_IMG = [
-        'Among-us' => '',
-        'Horizon-zero-dawn' => '',
-        'Rocket-league' => '',
-        'Ps5' => '',
-        'Warcraft' => ''
-    ]; */
+    public const ADS_IMG = [
+        'Among-us' => 'amongus.png',
+        'Horizon-zero-dawn' => 'horizon.png',
+        'Rocket-league' => 'rocket.png',
+        'Ps5' => 'ps5.png',
+        'Warcraft' => 'warcraft.png'
+    ];
 
     public function load(ObjectManager $manager): void
     {
-        for ($i = 1; $i <= 5; $i++) {
+        foreach (self::ADS_IMG as $title => $pics) {
             $advert = new Advertisement();
-            $advert->setName('LOL');
-            $advert->setPoster(self::ADS_IMG);
+            $advert->setName($title);
+            $advert->setPoster($pics);
             $advert->setLinkTo('https://www.leagueoflegends.com/fr-fr/');
             $manager->persist($advert);
         }
