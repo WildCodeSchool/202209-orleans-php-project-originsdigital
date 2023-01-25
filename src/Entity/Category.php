79c664c87eb4a039ba4600af079243290614df83
Severine Helene Vilaine <severine.vilaine@gmail.com>
2023-01-25 09:52:24 +0100

    Category Upload works - Twig render to come

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,13 +2,20 @@
 
 namespace App\Entity;
 
-use App\Repository\CategoryRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Video;
+use DateTime;
+use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\DBAL\Types\Types;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[Vich\Uploadable]
 class Category
 {
     #[ORM\Id]
@@ -23,6 +30,20 @@ class Category
 
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\Length(max: 255)]
+    private ?string $background = null;
+
+    #[Vich\UploadableField(mapping: 'backgrounds', fileNameProperty: 'background')]
+    #[Assert\File(
+        maxSize: '2M',
+        mimeTypes: ['image/jpeg', 'image/png', 'image/webp'],
+    )]
+    private ?File $backgroundPicture = null;
+
+    #[ORM\Column(type: types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updatedAt = null;
 
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Video::class)]
     private Collection $video;
@@ -69,6 +90,34 @@ class Category
         return $this->video;
     }
 
+    public function setBackgroundPicture(?File $backgroundPicture = null): void
+    {
+        $this->backgroundPicture = $backgroundPicture;
+
+        if (null !== $backgroundPicture) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new DateTime('now');
+        }
+    }
+
+    public function getBackgroundPicture(): ?File
+    {
+        return $this->backgroundPicture;
+    }
+
+    public function getBackground(): ?string
+    {
+        return $this->background;
+    }
+
+    public function setBackground(?string $background): self
+    {
+        $this->background = $background;
+
+        return $this;
+    }
+
     public function addVideo(Video $video): self
     {
         if (!$this->video->contains($video)) {
@@ -87,6 +136,18 @@ class Category
                 $video->setCategory(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
