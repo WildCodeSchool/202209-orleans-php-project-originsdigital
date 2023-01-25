0db392e81eca1122e6813f3389ad96e522f31942
lucianpurcaru <[lucian.purcaru@gmail.com](mailto:lucian.purcaru@gmail.com)>
2023-01-25 14:44:17 +0100

    like videos working

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -4,12 +4,13 @@ namespace App\Entity;
 
 use DateTime;
 use DateTimeInterface;
-use App\Repository\VideoRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\VideoRepository;
+use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\HttpFoundation\File\File;
+use Doctrine\Common\Collections\ArrayCollection;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -70,9 +71,15 @@ class Video
     #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'favorite')]
     private Collection $favorite;
 
+    #[ORM\ManyToMany(targetEntity: User::class)]
+    #[JoinTable(name: 'user_video_likes')]
+    private Collection $likes;
+
+
     public function __construct()
     {
         $this->favorite = new ArrayCollection();
+        $this->likes = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -244,5 +251,34 @@ class Video
         $this->favorite->removeElement($favorite);
 
         return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getLikes(): Collection
+    {
+        return $this->likes;
+    }
+
+    public function addLike(User $like): self
+    {
+        if (!$this->likes->contains($like)) {
+            $this->likes->add($like);
+        }
+
+        return $this;
+    }
+
+    public function removeLike(User $like): self
+    {
+        $this->likes->removeElement($like);
+
+        return $this;
+    }
+
+    public function isLikedByUser(User $user): bool
+    {
+        return $this->likes->contains($user);
     }
 }
