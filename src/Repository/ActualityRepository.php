7032e70a13b2fef9c37c0d67ce6503fc6a9b3931
Kevyn SOLA <kevyn.sola@gmail.com>
2023-01-25 11:24:01 +0100

    add actuality entity

---
+++ src/Repository/ActualityRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Actuality;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Actuality>
+ *
+ * @method Actuality|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Actuality|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Actuality[]    findAll()
+ * @method Actuality[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ActualityRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Actuality::class);
+    }
+
+    public function save(Actuality $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Actuality $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Actuality[] Returns an array of Actuality objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Actuality
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
