831f3ded96f2f6f441dde9362bd69922a3ca87f3
Severine Helene Vilaine <severine.vilaine@gmail.com>
2023-02-07 17:20:44 +0100

    Added some users

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -28,6 +28,46 @@ class UserFixtures extends Fixture
         $contributor->setLastName('Doe');
         $manager->persist($contributor);
 
+        $contributor = new User();
+        $contributor->setEmail('user_2@mail.com');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $hashedPassword = $this->passwordHasher->hashPassword($contributor, 'user-2password');
+        $contributor->setPassword($hashedPassword);
+        $contributor->setUserName('emily-in-paris');
+        $contributor->setFirstName('Emily');
+        $contributor->setLastName('Cooper');
+        $manager->persist($contributor);
+
+        $contributor = new User();
+        $contributor->setEmail('user_3@mail.com');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $hashedPassword = $this->passwordHasher->hashPassword($contributor, 'user-3password');
+        $contributor->setPassword($hashedPassword);
+        $contributor->setUserName('kiki45');
+        $contributor->setFirstName('Mark');
+        $contributor->setLastName('Zuzu');
+        $manager->persist($contributor);
+
+        $contributor = new User();
+        $contributor->setEmail('user-4@mail.com');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $hashedPassword = $this->passwordHasher->hashPassword($contributor, 'user-4password');
+        $contributor->setPassword($hashedPassword);
+        $contributor->setUserName('sblondeau');
+        $contributor->setFirstName('Sylvain');
+        $contributor->setLastName('Blondeau');
+        $manager->persist($contributor);
+
+        $contributor = new User();
+        $contributor->setEmail('user-5@mail.com');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $hashedPassword = $this->passwordHasher->hashPassword($contributor, 'user-5password');
+        $contributor->setPassword($hashedPassword);
+        $contributor->setUserName('macrohard');
+        $contributor->setFirstName('Billy');
+        $contributor->setLastName('Laporte');
+        $manager->persist($contributor);
+
         $admin = new User();
         $admin->setEmail('admin@zesport.com');
         $admin->setRoles(['ROLE_ADMIN']);
